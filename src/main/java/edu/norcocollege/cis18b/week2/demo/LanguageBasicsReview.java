e6d5ab42c4029ee997c1286b459e18ae45b4be31
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-04 03:15:53 +0000

    Added Constructors & Basic Methods for LanguageBasics Review

--- src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
@@ -1,5 +1,5 @@
 package edu.norcocollege.cis18b.week2.demo;
-
+import java.util.List;
 /**
  * This class is a review of basic Java language features. It’s not
  * meant to be comprehensive, but it should cover the basics and provide
@@ -12,7 +12,48 @@ package edu.norcocollege.cis18b.week2.demo;
  *  - Demonstrate basic OOP concepts (classes, objects, inheritance)
  */
 public class LanguageBasicsReview {
+    //Private Variables
+    private String dinosaurName;
+    //no-arg Constuctor
+    public LanguageBasicsReview(){
+        this.dinosaurName="Dinosaur";
+    }
+    //one-arg Constuctor
+    public LanguageBasicsReview(String dinosaur){
+        this.dinosaurName=dinosaur;
+    }
+    //Roar Method
+    public void roar(){
+        System.out.println(dinosaurName+"ROARS LOUDLY!");
+    }
+    //isExtinct Method
+    public static boolean isExtinct(int year){
+        return year<65_000_000;
+    }
+    //Overloaded Eat Methods
+    public int eat(int plants){
+        return plants*2;
+    }
+    public double eat(double lbsOfMeat){
+        return lbsOfMeat*2.5;
+    }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Demonstrate All Features
     public void demonstrate() {
         // TODO: Implement demonstration of basic Java features
         // NOTE: Remove the exception below when you've implemented your code
